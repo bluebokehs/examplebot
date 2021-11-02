c2abc5381c26a3127ed40ca70df0fedd8fd02828
Owen Kern <owen@kernkreations.com>
2021-11-02 11:01:54 -0400

    deleted whitespace

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   consumer_key:         'blah',
   consumer_secret:      'blah',
   access_token:         'blah',
-  access_token_secret:  'blah' 
+  access_token_secret:  'blah'
 }
