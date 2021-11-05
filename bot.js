20b243059968477607141c1bfe0e2c8dcd06f251
Owen Kern <owen@kernkreations.com>
2021-11-05 16:02:25 -0500

    Implemented posting

--- bot.js
+++ bot.js
@@ -17,15 +17,38 @@ function retweetLatest() {
 	  	// ...then we grab the ID of the tweet we want to retweet...
 		var retweetId = data.statuses[0].id_str;
 		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+		
+
+		T.post('statuses/retweets/' + retweetId, { }, function (error, response) {
 			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
+				console.log('Success! Check your bot, it should have tweeted something.')
 			}
 			// If there was an error with our Twitter call, we print it out here.
 			if (error) {
 				console.log('There was an error with Twitter:', error);
 			}
 		})
+		
+		T.post('favorites/create', {id:retweetId}, function(err, data, response){console.log("just liked a post")});
+		console.log(data);
+
+		T.get('followers/ids', { screen_name: 'nocontextsquidg'}, function(err, data, response) {
+			console.log(data)
+
+			var randomNum = Math.floor(Math.random() * 101)
+
+			var account = data.statuses[randomNum].id_str;
+
+			var tweet = {
+				status: '@' + account + ' has a ' + randomNum + ' chance to win Squid Game'
+			}
+	
+			T.post('statuses/update', tweet, function(err, data, response) {
+				console.log(data)
+			})
+		})
+
+		
 	  }
 	  // However, if our original search request had an error, we want to print it out here.
 	  else {
@@ -38,4 +61,4 @@ function retweetLatest() {
 retweetLatest();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+setInterval(retweetLatest, 1000 * 60 * 60 * 6);
