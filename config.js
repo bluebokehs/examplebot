8a79ab957ef176ed5882ff19e2f324ca641485ba
Ramya Ramaswamy <90233199+ramyaramaswamy21@users.noreply.github.com>
2021-11-05 17:35:36 -0400

    Add files via upload
    11/5 upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         'ihddrdbvXNyLQHQTaxS7NjTdZ',
+  consumer_secret:      'Jk75Ec919e0sTgn9Ec0jDLMRpuP6cMizvcxCrTZrIWgyEEYpnU',
+  access_token:         '906938290649083904-31CL5Mwr4AX5B8Ap2xe4GlY31rnsqbc',
+  access_token_secret:  'NnhvJ2S0BS0JzKMGRjJ5CnPHpcFXsKa2fyoPNx4YdfQHr'
 }
