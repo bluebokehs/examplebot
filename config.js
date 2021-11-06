4f20211c1ee3cbf4b3f662319a04a9adb9819760
JacksonLeone <jackson.leone.usa@gmail.com>
2021-11-06 17:36:55 -0400

    Correct Config File

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key: 'l08gvWk9QxMWVDak2Otjy7vqx',
-  consumer_secret: 'NHowWhVXod1efPN7vwSTmIxNWy9uprEsdGr56MI381jQyYTwt4',
-  access_token: '906938290649083904-kQ3MHpNdkF4Cq3OSHQAtr3D6ks3Okvq',
-  access_token_secret: 'N9mOoRlQu7UkKSntfCv06ZGReOQrhKv3ZYmV0cAkDUopx'
+  consumer_key: '7e5kngNmS0ghdSJjQzfNYim91',
+  consumer_secret: 'SKp2u1W2rkZWxnWOCqr9WCkCSGx3KcUQweAe9kjlu0fuM7bqfL',
+  access_token: '906938290649083904-PtNk2WBsIATLo56SpWRzR5GUpZ7nY8Z',
+  access_token_secret: 'rYadceSEkXM9G6yibSuiZr8jowEXSmdxdOpGFlY8r0QIz'
 }
