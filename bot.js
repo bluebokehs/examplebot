eb81cae6e36d80da922e84044a57806f932bc55e
JacksonLeone <jackson.leone.usa@gmail.com>
2021-11-06 17:36:23 -0400

    Current working version of Bot

--- bot.js
+++ bot.js
@@ -10,34 +10,6 @@ var squidGameSearch = {q: "#SquidGame", count: 10, result_type: "recent" };
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
 	T.get('search/tweets', squidGameSearch, function (error, data) {
-<<<<<<< Updated upstream
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log(retweetId)
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-
-		T.post('favorites/create', { id: retweetId }, function (err, data, response) { 
-			console.log("just liked a post") });
-			console.log(data);
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-=======
 		// log out any errors and responses
 		console.log(error, data);
 		// If our search request to the server had no errors...
@@ -90,7 +62,6 @@ function retweetLatest() {
 				console.log(data)
 			})
 		}
->>>>>>> Stashed changes
 	});
 }
 
