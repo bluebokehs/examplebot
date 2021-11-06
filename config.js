968cc241866b99e23933c9ce5facf9a771e36026
JacksonLeone <jackson.leone.usa@gmail.com>
2021-11-06 00:20:53 -0400

    updated Keys

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key: 'l08gvWk9QxMWVDak2Otjy7vqx',
+  consumer_secret: 'NHowWhVXod1efPN7vwSTmIxNWy9uprEsdGr56MI381jQyYTwt4',
+  access_token: '906938290649083904-kQ3MHpNdkF4Cq3OSHQAtr3D6ks3Okvq',
+  access_token_secret: 'N9mOoRlQu7UkKSntfCv06ZGReOQrhKv3ZYmV0cAkDUopx'
 }
