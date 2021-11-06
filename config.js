49dae0e25c4a247b86ee212fbea3a38098e00590
Ramya Ramaswamy <90233199+ramyaramaswamy21@users.noreply.github.com>
2021-11-05 23:43:35 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'ihddrdbvXNyLQHQTaxS7NjTdZ',
-  consumer_secret:      'Jk75Ec919e0sTgn9Ec0jDLMRpuP6cMizvcxCrTZrIWgyEEYpnU',
-  access_token:         '906938290649083904-31CL5Mwr4AX5B8Ap2xe4GlY31rnsqbc',
-  access_token_secret:  'NnhvJ2S0BS0JzKMGRjJ5CnPHpcFXsKa2fyoPNx4YdfQHr'
+  consumer_key:         'l08gvWk9QxMWVDak2Otjy7vqx',
+  consumer_secret:      'NHowWhVXod1efPN7vwSTmIxNWy9uprEsdGr56MI381jQyYTwt4',
+  access_token:         '906938290649083904-kQ3MHpNdkF4Cq3OSHQAtr3D6ks3Okvq',
+  access_token_secret:  'N9mOoRlQu7UkKSntfCv06ZGReOQrhKv3ZYmV0cAkDUopx'
 }
